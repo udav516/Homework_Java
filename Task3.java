0ac62f5bd03812574bc4d16c32cfe9bd97ba9b86
udav516 <eldar.udav516@yandex.ru>
2022-10-21 17:48:39 +0300

    add Task 3

--- Task3.java
+++ Task3.java
@@ -2,12 +2,52 @@
 
 // 2.Пусть дан произвольный список целых чисел, удалить из него четные числа
 
-// 3.Задан целочисленный список ArrayList. 
+// 3.Задан целочисленный список ArrayList.
 // Найти минимальное, максимальное и среднее из этого списка.
 
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Task3 {
-    public static void main(String[] args) {
-        
+
+    public static void main(String[] args) throws IOException {
+        int[] sortArr = { 12, 6, 4, 1, 15, 10 };
+        int[] result = mergeSort(sortArr);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] mergeSort(int[] sortArr) {
+        int[] buffer1 = Arrays.copyOf(sortArr, sortArr.length);
+        int[] buffer2 = new int[sortArr.length];
+        return mergeSortInner(buffer1, buffer2, 0, sortArr.length);
+    }
+
+    public static int[] mergeSortInner(int[] buffer1, int[] buffer2, int startIndex, int endIndex) {
+        if (startIndex >= endIndex - 1) {
+            return buffer1;
+        }
+
+        // уже отсортирован
+        int middle = startIndex + (endIndex - startIndex) / 2;
+        int[] sorted1 = mergeSortInner(buffer1, buffer2, startIndex, middle);
+        int[] sorted2 = mergeSortInner(buffer1, buffer2, middle, endIndex);
+
+        // слияние
+        int index1 = startIndex;
+        int index2 = middle;
+        int destIndex = startIndex;
+        int[] result = sorted1 == buffer1 ? buffer2 : buffer1;
+        while (index1 < middle && index2 < endIndex) {
+            result[destIndex++] = sorted1[index1] < sorted2[index2]
+                    ? sorted1[index1++]
+                    : sorted2[index2++];
+        }
+        while (index1 < middle) {
+            result[destIndex++] = sorted1[index1++];
+        }
+        while (index2 < endIndex) {
+            result[destIndex++] = sorted2[index2++];
+        }
+        return result;
     }
 }
-
