50dece0d922f25fd91c5c61d1bc1beb4937b67b9
udav516 <eldar.udav516@yandex.ru>
2022-10-22 16:12:59 +0300

    add Delete even element

--- Task3.java
+++ Task3.java
@@ -1,19 +1,35 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Logger;
+// (ВАЖНО) Для вывода необходимо использовать логгер(Java.Util.Logging)
+
 // 1.Реализовать алгоритм сортировки слиянием
 
 // 2.Пусть дан произвольный список целых чисел, удалить из него четные числа
 
 // 3.Задан целочисленный список ArrayList.
 // Найти минимальное, максимальное и среднее из этого списка.
-
-import java.io.IOException;
-import java.util.Arrays;
-
 public class Task3 {
 
     public static void main(String[] args) throws IOException {
-        int[] sortArr = { 12, 6, 4, 1, 15, 10 };
+        int[] sortArr = { 12, 6, 3, 1, 13, 10 };
         int[] result = mergeSort(sortArr);
-        System.out.println(Arrays.toString(result));
+        // System.out.println(Arrays.toString(result));
+        Logger logger = Logger.getAnonymousLogger();
+        logger.info(Arrays.toString(result));
+        List<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(11);
+        numbers.add(42);
+        numbers.add(17);
+        numbers.add(32);
+        numbers.add(55);
+        deleteEvenElement(numbers);
+        String listString = numbers.toString();
+        // System.out.println(listString);
+        logger.info(listString);
     }
 
     public static int[] mergeSort(int[] sortArr) {
@@ -27,12 +43,10 @@ public class Task3 {
             return buffer1;
         }
 
-        // уже отсортирован
         int middle = startIndex + (endIndex - startIndex) / 2;
         int[] sorted1 = mergeSortInner(buffer1, buffer2, startIndex, middle);
         int[] sorted2 = mergeSortInner(buffer1, buffer2, middle, endIndex);
 
-        // слияние
         int index1 = startIndex;
         int index2 = middle;
         int destIndex = startIndex;
@@ -49,5 +63,18 @@ public class Task3 {
             result[destIndex++] = sorted2[index2++];
         }
         return result;
+    }
+ 
+    public static List<Integer> deleteEvenElement(List<Integer> sortArr) {
+        for (Iterator<Integer> iterator = sortArr.iterator(); iterator.hasNext();) {
+            Integer number = iterator.next();
+            if (number % 2 == 0) {
+                iterator.remove();
+            }
+        
+        }
+        
+        // System.out.println("Sorted List: " + sortArr);
+        return sortArr;
     }
 }
